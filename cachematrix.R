af2a5dc85c1c03247a5de63fe2c77330ba795c06
Gvadakpat <gvadakpat@yahoo.com>
2015-07-21 20:12:05 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,7 @@
 ## makeCacheMatrix creates a special matrix object that caches its inverse
 ## cacheSolve computes the inverse of special matrix returned by makeCacheMatrix
+## If the matrix inverse has already been calculated it will find it in the cache
+## and return it, if not it will calculate again
 
 ## Write a short comment describing this function
 
